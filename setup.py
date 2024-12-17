9c873e730cf1008990f713ce2d35ac234192beb6
rodrigo <rodrigo.magati@hotmail.com>
2024-12-17 16:31:45 -0300

    Update setup.py
    sas

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 
 setup(
-    name="image_processing",
+    name="image_processing_dio",
     version="0.0.1",
     author="Rodrigo",
     description="Image Processing Package using Skimage",
